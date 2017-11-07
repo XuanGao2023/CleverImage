5ee6322879f0156117ad7f754c860dbebbfa541a
pan <panda.gao@qq.com>
2017-11-07 15:22:32 +0800

    Add CircleImageView to implement the circle feature of CleverImageView

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # CleverImage
 
-This is a library for getting pic and showing pic with disk cache and memory cache on
-Android devices.
+This is a library for getting pic and showing pic with disk cache and
+memory cache on Android devices.
--- app/src/main/java/com/pan/testapp/MainActivity.java
+++ app/src/main/java/com/pan/testapp/MainActivity.java
@@ -7,16 +7,18 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.pan.cleverimage.CleverImageView;
 import com.pan.cleverimage.ImageGetter;
 
 public class MainActivity extends AppCompatActivity {
 	private TextView textViewTest;
-	private ImageView imageViewTest;
-	private static final String FILE_PATH = "https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=522425600,1333123193&fm=173&s=9225BD08EA322A8EF73D7401030060C9&w=218&h=146&img.JPEG";
-
+	private CleverImageView cleverImageView;
+	private static final String FILE_URL0 = "https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/" +
+            "u=522425600,1333123193&fm=173&s=9225BD08EA322A8EF73D7401030060C9&w=218&h=146&img.JPEG";
+    private static final String FILE_URL1 = "https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/" +
+            "u=2861671602,4205204930&fm=173&s=50019D5786616CA4793D90CB03008031&w=218&h=146&img.JPEG";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -25,19 +27,32 @@ public class MainActivity extends AppCompatActivity {
 
 		ImageGetter.initInstance();
 
-		imageViewTest = (ImageView) findViewById(R.id.imageViewTest);
-		findViewById(R.id.btnGetPic).setOnClickListener(new View.OnClickListener() {
+		cleverImageView = (CleverImageView) findViewById(R.id.cleverImageView);
+		findViewById(R.id.btnGetPic0).setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				ImageGetter.getPic(FILE_PATH, new ImageGetter.ImageGotListener() {
+				ImageGetter.getPic(FILE_URL0, new ImageGetter.ImageGotListener() {
 					@Override
 					public void OnImageGot(Bitmap bitmap) {
-						System.out.println("OnImageGot: " + FILE_PATH + bitmap);
-						imageViewTest.setImageBitmap(bitmap);
+						System.out.println("OnImageGot: " + FILE_URL0 + bitmap);
+						cleverImageView.setImageBitmap(bitmap);
 					}
 				});
 			}
 		});
+        findViewById(R.id.btnGetPic1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageGetter.getPic(FILE_URL1, new ImageGetter.ImageGotListener() {
+                    @Override
+                    public void OnImageGot(Bitmap bitmap) {
+                        System.out.println("OnImageGot: " + FILE_URL1 + bitmap);
+                        cleverImageView.setImageBitmap(bitmap);
+                    }
+                });
+            }
+        });
+
 		findViewById(R.id.btnClearMemCache).setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -50,6 +65,18 @@ public class MainActivity extends AppCompatActivity {
 				ImageGetter.getInstance().clearDiskCache();
 			}
 		});
+		findViewById(R.id.btnCircle).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cleverImageView.setDisableCircularTransformation(false);
+            }
+        });
+        findViewById(R.id.btnDisableCircle).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cleverImageView.setDisableCircularTransformation(true);
+            }
+        });
 	}
 
 	public void checkPermission() {
